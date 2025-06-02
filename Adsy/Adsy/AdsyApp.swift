aeff462ae8289b57877cea45659d03ffa5b9e052
Samet Berberoglu <SametBerberoglu01@gmail.com>
2025-06-02 09:30:04 +0200

    Initial commit of the project

---
+++ Adsy/Adsy/AdsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsyApp.swift
+//  Adsy
+//
+//  Created by Samet Berberoğlu on 2025-06-02.
+//
+
+import SwiftUI
+
+@main
+struct AdsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
